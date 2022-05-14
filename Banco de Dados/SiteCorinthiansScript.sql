b688360be992b2e105b55f9cd5e69f8b2223baa5
Matheus <Matheus.gregorin@sptech.school>
2022-05-14 01:45:36 -0300

    atualizações 1.0

--- Banco de Dados/SiteCorinthiansScript.sql
+++ Banco de Dados/SiteCorinthiansScript.sql
@@ -1,6 +1,15 @@
 create database site;
 use site;
 
+create table usuarios (
+idUsuario int primary key auto_increment,
+nomeUsuario varchar (25),
+email varchar(50),
+senha varchar (10),
+fk_jogador int,
+foreign key (fk_jogador) references jogadores(idJogador)
+);
+
 create table jogadores (
 idJogador int primary key auto_increment,
 nomeJogador varchar (45),
@@ -13,20 +22,27 @@ numeroJogos varchar(5),
 golsSofridos varchar(3)
 );
 
-
-create table usuarios (
-idUsuario int primary key auto_increment,
-nomeUsuario varchar (25),
-email varchar(50),
-senha varchar (10),
+create table conquistas (
+fk_trofeu int,
+foreign key (fk_trofeu) references trofeis(idTrofeu),
 fk_jogador int,
-foreign key (fk_jogador) references jogadores(idJogador)
+foreign key (fk_jogador) references jogadores(idJogador),
+dtConquista date
 );
 
-select * from jogadores;
+create table trofeis (
+idTrofeu int primary key auto_increment,
+nomeTitulo varchar (50)
+);
+
 select * from usuarios;
+select * from jogadores;
+select * from conquistas;
+select * from trofeis;
 
 drop table jogadores;
 drop table usuarios;
+drop table conquistas;
+drop table trofeis;
 
-insert into jogadores values (1, 'Cassio', '34', 'Goleiro', '2012-03-28', '0', '0', '585', null);
+insert into jogadores values (1, 'Cassio', '34', 'Goleiro', '2012-03-28', '0', '0', '585', '712');
