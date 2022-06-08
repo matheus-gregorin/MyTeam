4a86ae3eba8c35c5452f81b4928660cf20fe510e
Matheus <Matheus.gregorin@sptech.school>
2022-06-08 18:49:56 -0300

    Banco Oficial

--- Banco de Dados/SiteCorinthiansScript.sql
+++ Banco de Dados/SiteCorinthiansScript.sql
@@ -1,4 +1,5 @@
 create database site;
+
 use site;
 
 create table usuarios (
@@ -30,12 +31,12 @@ foreign key (fk_jogador) references jogadores(idJogador),
 dtConquista date
 );
 
+
 create table trofeis (
 idTrofeu int primary key auto_increment,
 nomeTitulo varchar (50)
 );
 
-SELECT * FROM usuarios WHERE email = 'math@gmail.com.br' AND senha = '123456';
 
 select * from usuarios;
 select * from jogadores;
@@ -47,37 +48,23 @@ select * from trofeis;
 #drop table conquistas;
 #drop table trofeis;
 
+#Insert Site
 
-
-insert into usuarios values (1, 'matheus', 'math@gmail.com.br', '123456', 1),
-                            (2, 'matheus', 'math@gmail.com.br', '123457', 2),
-                            (3, 'matheus', 'math@gmail.com.br', '123458', 3),
-                            (4, 'matheus', 'math@gmail.com.br', '123459', 4),
-                            (5, 'matheus', 'math@gmail.com.br', '123451', 5),
-                            (6, 'matheus', 'math@gmail.com.br', '123452', 6),
-                            (7, 'matheus', 'math@gmail.com.br', '123453', 7),
-                            (8, 'matheus', 'math@gmail.com.br', '123456', 8),
-                            (9, 'matheus', 'math@gmail.com.br', '123456', 9),
-                            (10, 'matheus', 'math@gmail.com.br', '123456', 10),
-                            (11, 'matheus', 'math@gmail.com.br', '123456', 11),
-                            (12, 'matheus', 'math@gmail.com.br', '123456', 12),
-                            (13, 'matheus', 'math@gmail.com.br', '123456', 13),
-                            (14, 'matheus', 'math@gmail.com.br', '123456', 7),
-                            (15, 'matheus', 'math@gmail.com.br', '123456', 10);
-                            
-insert into usuarios values (16, 'matheus', 'math@gmail.com.br', '123416', 10),
-                            (17, 'matheus', 'math@gmail.com.br', '123426', 10),
-                            (18, 'matheus', 'math@gmail.com.br', '123436', 10),
-                            (19, 'matheus', 'math@gmail.com.br', '123446', 10),
-                            (20, 'matheus', 'math@gmail.com.br', '123456', 7),
-                            (21, 'matheus', 'math@gmail.com.br', '123466', 7),
-                            (22, 'matheus', 'math@gmail.com.br', '123476', 7);
-                            
-insert into usuarios values (23, 'matheus', 'math@gmail.com.br', '123316', 10),
-                            (24, 'matheus', 'math@gmail.com.br', '123416', 10),
-                            (25, 'matheus', 'math@gmail.com.br', '123516', 10);
-                            
-                            
+insert into usuarios values (1, 'matheus1', 'math1@gmail.com.br', '123456', 1),
+                            (2, 'matheus2', 'math2@gmail.com.br', '123457', 2),
+                            (3, 'matheus3', 'math3@gmail.com.br', '123458', 3),
+                            (4, 'matheus4', 'math4@gmail.com.br', '123459', 4),
+                            (5, 'matheus5', 'math5@gmail.com.br', '123451', 5),
+                            (6, 'matheus6', 'math6@gmail.com.br', '123452', 6),
+                            (7, 'matheus7', 'math7@gmail.com.br', '123453', 7),
+                            (8, 'matheus8', 'math8@gmail.com.br', '123456', 8),
+                            (9, 'matheus9', 'math9@gmail.com.br', '123456', 9),
+                            (10, 'matheus10', 'math10@gmail.com.br', '123456', 10),
+                            (11, 'matheus11', 'math11@gmail.com.br', '123456', 11),
+                            (12, 'matheus12', 'math12@gmail.com.br', '123456', 12),
+                            (13, 'matheus13', 'math13@gmail.com.br', '123456', 13),
+                            (14, 'matheus14', 'math14@gmail.com.br', '123456', 7),
+                            (15, 'matheus15', 'math15@gmail.com.br', '123456', 10);
                             
 
 insert into jogadores values (1, 'Cassio', '34', 'Goleiro', '2012-03-28', '0', '0', '585', '476'),
@@ -94,10 +81,21 @@ insert into jogadores values (1, 'Cassio', '34', 'Goleiro', '2012-03-28', '0', '
                              (12, 'Roger Guedes', '25', 'Atacante', '2021-07-21', '6', '4', '22', '0'),
                              (13, 'Jo', '35', 'Atacante', '2003-06-19', '3', '0', '160', '0');
                              
+                             
+insert into trofeis values (null, "libertadores"),
+                           (null, "Mundial");
+                           
+insert into conquistas values (1, 1, "2012-06-28"),
+							  (1, 2, "2012-06-28"),
+                              (1, 3, "2012-06-28"),
+                              (1, 4, "2012-06-28");
+                             
 
 
 
 # Select Site
+
+SELECT * FROM usuarios WHERE email = 'math@gmail.com.br' AND senha = '123456';
 
 select jogadores.nomeJogador, COUNT(usuarios.fk_jogador)
 from usuarios
